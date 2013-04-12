27305e798ad7645517b63deb6166cb0a8cd13201
Michael Klein <mbklein@gmail.com>
2013-04-12 07:58:50 -0500

    Make matterhorn a system user; fix init script

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,39 +1,40 @@
 class matterhorn::install(
-	$treeish = "1.4.x"
+  $treeish = "1.4.x"
 ) {
-	include staging
+  include staging
 
-	group { 'matterhorn':
-		ensure    => present,
-	}
+  group { 'matterhorn':
+    ensure    => present,
+    system    => true
+  }
 
   user { 'matterhorn':
     ensure    => present,
     gid       => 'matterhorn',
     system    => true,
-    require 	=> Group['matterhorn']
+    require   => Group['matterhorn']
   }
 
   staging::file { 'matterhorn-1.4.tar.gz':
-  	subdir    => 'matterhorn',
-  	source		=> "https://github.com/avalonmediasystem/avalon-felix/archive/${treeish}.tar.gz"
-	}
+    subdir    => 'matterhorn',
+    source    => "https://github.com/avalonmediasystem/avalon-felix/archive/${treeish}.tar.gz"
+  }
 
-	file { '/usr/local/matterhorn':
-		ensure    => directory,
-		owner     => 'matterhorn',
-		group 	  => 'matterhorn',
-		mode      => '0775'
-	}
-	
-	exec { 'extract-matterhorn':
-		command   => "/bin/tar xzf ${staging::path}/matterhorn/matterhorn-1.4.tar.gz --strip-components 1",
-		cwd       => "/usr/local/matterhorn",
-		creates   => "/usr/local/matterhorn/lib",
-		user      => "matterhorn",
-		group 	  => "matterhorn",
-		require   => [File['/usr/local/matterhorn'],User['matterhorn'],Staging::File['matterhorn-1.4.tar.gz']]
-	}
+  file { '/usr/local/matterhorn':
+    ensure    => directory,
+    owner     => 'matterhorn',
+    group     => 'matterhorn',
+    mode      => '0775'
+  }
+  
+  exec { 'extract-matterhorn':
+    command   => "/bin/tar xzf ${staging::path}/matterhorn/matterhorn-1.4.tar.gz --strip-components 1",
+    cwd       => "/usr/local/matterhorn",
+    creates   => "/usr/local/matterhorn/lib",
+    user      => "matterhorn",
+    group     => "matterhorn",
+    require   => [File['/usr/local/matterhorn'],User['matterhorn'],Staging::File['matterhorn-1.4.tar.gz']]
+  }
 
 }
 
